f82c0c1493f7b0bd75a47395bb4593b637b68f41
Wojciech Zaremba <woj.zaremba@gmail.com>
2012-02-10 10:05:09 +0100

    just moved

---
+++ apps/liszt/src/ppl/apps/fem/Fem.scala
@@ -0,0 +1,180 @@
+package ppl.apps.liszt.dynamicMesh
+
+import ppl.apps.liszt.lib._
+import ppl.dsl.deliszt._
+import ppl.dsl.deliszt.MetaInteger._
+
+object FemRunner extends DeLisztApplicationRunner with Fem
+
+trait Fem extends DeLisztApplication with Libs {
+
+  var matrixE: Rep[Field[Edge, Float]] = null
+  var matrixV: Rep[Field[Vertex, Float]] = null
+/*
+  var xk: Rep[Field[Vertex, Float]] = null
+  var pk: Rep[Field[Vertex, Float]] = null
+  var rk: Rep[Field[Vertex, Float]] = null
+  var temp: Rep[Field[Vertex, Float]] = null*/
+
+  var values: Rep[Field[Vertex, Float]] = null
+  var values2: Rep[Field[Vertex, Float]] = null  
+  
+  var dF: Rep[Field[Vertex, Float]] = null
+  val sizex: Float = 4.f
+  val sizey: Float = 4.f
+
+  def f(x: Rep[Float], y: Rep[Float], z: Rep[Float]): Rep[Float] = x * (sizex - x) * y * (sizey - y) * 100.f 
+    //x * (sizex - x) * y * y * (sizey - y) * 10.f
+
+  def deltaF(x: Rep[Float], y: Rep[Float]): Rep[Float] = ( y * (sizey - y) + x * (sizex - x) ) * 200.f
+    //y * y * (sizey - y) * 20.f + x * (sizex - x) * (60.f * y - 20.f * sizey)
+
+  def lamda(v: Rep[Vertex], face: Rep[Face]): Rep[Vec[_3, Float]] = {
+    val set = vertices(face).filter((x: Rep[Vertex]) => ID(x) != ID(v))
+    val vec = cross((set(1) - set(0)), Vec(0.f, 0.f, 1.f)) / face.det
+    if (ID(v) == ID(vertex(face, 1)))
+      -vec
+    else
+      vec
+  }
+
+  def integralEdge(edge: Rep[Edge], face: Rep[Face]) = {
+    val a = head(edge)
+    val b = tail(edge)
+    if (m.inside.contains(a) && m.inside.contains(b)) {
+      dot(lamda(a, face), lamda(b, face))
+    } else 0.f
+  }
+
+  def integralVertex(a: Rep[Vertex], face: Rep[Face]) = {
+    if (m.inside.contains(a)) {
+      dot(lamda(a, face), lamda(a, face))
+    } else 0.f
+  }
+
+  def printV(mat: Rep[Field[Vertex, Float]]) {
+    println("Vertex Vector : ")
+    for (v <- vertices(m))
+      println(ID(v) + " " + mat(v))
+  }
+
+  def printE(mat: Rep[Field[Edge, Float]]) {
+    println("Matrix : ")
+    for (e <- edges(m))
+      println(ID(head(e)) + " " + ID(tail(e)) + " " + mat(e))
+  }
+  
+  def main(): Unit = {
+    m = RectangularMesh(sizex, sizey, 1.f)
+    pos = FieldWithLabel[Vertex, Vec[_3, Float]]("position", m)
+    //m = TetrahedronMesh()
+
+    matrixE = FieldWithConst[Edge, Float](0.f, m)
+    matrixV = FieldWithConst[Vertex, Float](0.f, m)
+//    xk = FieldWithConst[Vertex, Float](0.f, m)
+//    pk = FieldWithConst[Vertex, Float](0.f, m)
+//    rk = FieldWithConst[Vertex, Float](0.f, m)
+    dF = FieldWithConst[Vertex, Float](0.f, m)
+//    temp = FieldWithConst[Vertex, Float](0.f, m)
+    
+    values = FieldWithConst[Vertex, Float](0.f, m)
+    values2 = FieldWithConst[Vertex, Float](0.f, m)
+    
+    for (v <- vertices(m)) dF(v) = deltaF(v.x, v.y)
+
+    for (face <- faces(m)) {
+      for (v <- vertices(face)) {
+        matrixV(v) += integralVertex(v, face)
+      }
+      for (e <- edges(face))
+        matrixE(e) += integralEdge(e, face)
+    }
+
+    printE(matrixE)
+    println()
+    printV(matrixV)
+    OutputMesh.freeFem(m, "freefem.mesh")
+
+
+
+    /*val result: Rep[Int] = vertices(m).mapReduce((v: Rep[Vertex]) => ID(v))
+
+    println("result " + result)
+
+    for (v <- m.inside) {
+      rk(v) = dF(v)
+      pk(v) = dF(v)
+    }*/
+
+  /*  //var error: Rep[Float] = 100.f
+    //while (error > 0.00001f)
+    //for (i <- 1 until 10)
+  //  {
+      //println("iter " + i)
+      val alphaNominator: Rep[Float] = m.inside.mapReduce((v: Rep[Vertex]) => rk(v) * rk(v))
+      println("alphaNominator " + alphaNominator)
+
+      for (v <- m.inside) {
+        temp(v) = matrixV(v) * pk(v)
+        for (e <- edges(v)) {
+          val e2 = if (ID(head(e)) == ID(v)) tail(e) else head(e)
+          temp(v) += matrixE(e) * pk(e2)
+        }
+      }
+
+      
+      val alphaDenominator: Rep[Float] = m.inside.mapReduce((v: Rep[Vertex]) => pk(v) * temp(v))
+      println("alphaDenominator " + alphaDenominator)
+      val alpha: Rep[Float] = alphaNominator / alphaDenominator
+      println("alpha " + alphaNominator)
+      for (v <- m.inside) xk(v) += alpha * pk(v)
+      for (v <- m.inside) rk(v) -= alpha * temp(v)
+      val lenrk1 = m.inside.mapReduce((v: Rep[Vertex]) => rk(v) * rk(v))
+      println("lenrk1 " + lenrk1)
+      val beta: Rep[Float] = lenrk1 / alphaNominator
+      println("beta " + beta)
+      for (v <- m.inside) pk(v) *= beta
+      for (v <- m.inside) pk(v) += rk(v)      
+
+      println()
+      //error = alphaNominator
+      
+//    }*/
+    
+    
+    var error = 100.f
+    while (error > 0.0001f) 
+    {      
+      for (v <- m.inside) {
+        values2(v) = edges(v).mapReduce(
+           (e : Rep[Edge]) => (matrixE(e) * values(if (ID(e.head) == ID(v)) e.tail else e.head))
+          )        
+      }
+      for (v <- m.inside) {
+        values2(v) = (dF(v) - values2(v)) / matrixV(v)
+      }      
+      val error = vertices(m).mapReduce((v : Rep[Vertex]) => ( values2(v)-values(v) ) * ( values2(v)-values(v) ) )      
+      println("error " + error)     
+      for (v <- m.inside) {
+        values(v) = values2(v)
+      }
+      
+      
+      /*
+      for (v <- m.inside) {
+        values2(v) = dF(v)
+        for (e <- edges(v)) {
+          val e2 = if (ID(e.head) == ID(v)) e.tail else e.head
+          values2(v) -= (matrixE(e) * values(e2))
+        }   
+      }   
+      for (v <- m.inside) {
+        values(v) = values2(v) / matrixV(v)
+      }*/
+    }      
+ //   OutputMesh(m, "values.ply", values)
+   // OutputMesh(m, "f.ply", f _)
+    
+
+  }
+}
